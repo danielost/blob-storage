f4ab7a9aeaf9a376475c3a39416ae2a3d6c80ea3
Danylo Ostapchenko <danya.ostap@gmail.com>
2023-12-05 11:37:34 +0200

    Generate resources

---
+++ resources/model_blob_attributes.go
@@ -0,0 +1,13 @@
+/*
+ * GENERATED. Do not modify. Your changes might be overwritten!
+ */
+
+package resources
+
+import "time"
+
+type BlobAttributes struct {
+	CreatedAt time.Time `json:"created_at"`
+	// arbitrary text
+	Value string `json:"value"`
+}
