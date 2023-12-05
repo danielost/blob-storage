f72f945c99e587c37db428035e0fb77da10ca224
Danylo Ostapchenko <danya.ostap@gmail.com>
2023-12-05 19:41:39 +0200

    Add page params

---
+++ internal/service/handlers/page_params.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+
+	"gitlab.com/distributed_lab/kit/pgdb"
+	"gitlab.com/dl7850949/blob-storage/resources"
+)
+
+const (
+	pageParamLimit  = "page[limit]"
+	pageParamNumber = "page[number]"
+	pageParamCursor = "page[cursor]"
+	pageParamOrder  = "page[order]"
+)
+
+func GetOffsetLinks(r *http.Request, p pgdb.OffsetPageParams) *resources.Links {
+	result := resources.Links{
+		Next: getOffsetLink(r, p.PageNumber+1, p.Limit, p.Order),
+		Self: getOffsetLink(r, p.PageNumber, p.Limit, p.Order),
+	}
+
+	return &result
+}
+
+func getOffsetLink(r *http.Request, pageNumber, limit uint64, order string) string {
+	u := r.URL
+	query := u.Query()
+	query.Set(pageParamNumber, strconv.FormatUint(pageNumber, 10))
+	query.Set(pageParamLimit, strconv.FormatUint(limit, 10))
+	query.Set(pageParamOrder, order)
+	u.RawQuery = query.Encode()
+	return u.String()
+}
