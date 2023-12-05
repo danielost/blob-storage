2dbc0362a323d9459b3b86b1303a298ade3831c6
Danylo Ostapchenko <danya.ostap@gmail.com>
2023-12-05 11:38:04 +0200

    Add initial migration

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -1,3 +1,12 @@
 -- +migrate Up
 
+CREATE TABLE blobs (
+    id character(52) NOT NULL,
+    type integer NOT NULL,
+    value text NOT NULL,
+    created_at timestamp without time zone
+);
+
 -- +migrate Down
+
+DROP TABLE blobs
