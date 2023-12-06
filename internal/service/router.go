34eca155024642b52b81d2b3088e4248fee9f4fc
Danylo Ostapchenko <danya.ostap@gmail.com>
2023-12-06 19:21:04 +0200

    Update router paths

--- internal/service/router.go
+++ internal/service/router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-chi/chi"
 	"gitlab.com/distributed_lab/ape"
 	"gitlab.com/dl7850949/blob-storage/internal/data/pg"
+	"gitlab.com/dl7850949/blob-storage/internal/middleware"
 	"gitlab.com/dl7850949/blob-storage/internal/service/handlers"
 )
 
@@ -16,15 +17,27 @@ func (s *service) router() chi.Router {
 		ape.CtxMiddleware(
 			handlers.CtxLog(s.log),
 			handlers.CtxBlobsQ(pg.NewBlobsQ(s.db)),
+			handlers.CtxUsersQ(pg.NewUsersQ(s.db)),
 		),
 	)
+
 	r.Route("/integrations/blobs", func(r chi.Router) {
-		r.Post("/", handlers.CreateBlob)
-		r.Get("/", handlers.GetBlobsList)
-		r.Route("/{id}", func(r chi.Router) {
-			r.Get("/", handlers.GetBlob)
-			r.Delete("/", handlers.DeleteBlob)
+		// Protected routes
+		r.Route("/", func(r chi.Router) {
+			// Custom JWT middleware
+			r.Use(middleware.ValidateJWT)
+
+			r.Post("/", handlers.CreateBlob)
+			r.Get("/", handlers.GetBlobsList)
+			r.Route("/{id}", func(r chi.Router) {
+				r.Get("/", handlers.GetBlob)
+				r.Delete("/", handlers.DeleteBlob)
+			})
 		})
+
+		// Unprotected routes
+		r.Post("/signup", handlers.SignUp)
+		r.Post("/signin", handlers.SignIn)
 	})
 
 	return r
