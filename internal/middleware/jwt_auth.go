0b2e54ea2e92c21585a06cee2567c30365e4f09e
Danylo Ostapchenko <danya.ostap@gmail.com>
2023-12-06 19:12:46 +0200

    Refactor auth middleware

--- internal/middleware/jwt_auth.go
+++ internal/middleware/jwt_auth.go
@@ -8,15 +8,13 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 	"gitlab.com/distributed_lab/ape"
 	"gitlab.com/distributed_lab/ape/problems"
-	"gitlab.com/dl7850949/blob-storage/internal/service/handlers"
+	"gitlab.com/distributed_lab/logan/v3/errors"
 )
 
 func ValidateJWT(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		tokenString := r.Header.Get("x-jwt-token")
-		token, err := validateJWT(tokenString)
+		token, err := ParseJWT(r)
 		if err != nil || !token.Valid {
-			handlers.Log(r).Warn("failed to validate user")
 			ape.RenderErr(w, problems.Unauthorized())
 			return
 		}
@@ -25,8 +23,9 @@ func ValidateJWT(next http.Handler) http.Handler {
 	})
 }
 
-func validateJWT(tokenString string) (*jwt.Token, error) {
+func ParseJWT(r *http.Request) (*jwt.Token, error) {
 	secret := os.Getenv("JWT_SECRET")
+	tokenString := r.Header.Get("x-jwt-token")
 
 	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
@@ -35,4 +34,23 @@ func validateJWT(tokenString string) (*jwt.Token, error) {
 
 		return []byte(secret), nil
 	})
+}
+
+func GetIdFromJWT(r *http.Request) (int64, error) {
+	token, err := ParseJWT(r)
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to parse JWT")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return 0, errors.Wrap(err, "failed to retrive JWT claims")
+	}
+
+	userId, ok := claims["id"].(float64)
+	if !ok {
+		return 0, errors.Wrap(err, "JWT claims doesn't contain id")
+	}
+
+	return int64(userId), nil
 }
