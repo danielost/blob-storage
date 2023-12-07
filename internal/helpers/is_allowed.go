7c926cea75f2198f9e88a4121bb2f6bcd05f1b31
Danylo Ostapchenko <danya.ostap@gmail.com>
2023-12-07 12:58:15 +0200

    Change IsAllowed parameters

--- internal/helpers/is_allowed.go
+++ internal/helpers/is_allowed.go
@@ -2,18 +2,11 @@ package helpers
 
 import (
 	"fmt"
-	"net/http"
 
 	"gitlab.com/dl7850949/blob-storage/internal/data"
-	"gitlab.com/dl7850949/blob-storage/internal/middleware"
 )
 
-func IsAllowed(r *http.Request, blob data.Blob) error {
-	userId, err := middleware.GetIdFromJWT(r)
-	if err != nil {
-		return err
-	}
-
+func IsAllowed(userId int64, blob *data.Blob) error {
 	if blob.OwnerId != userId {
 		return fmt.Errorf("user is not the owner of a blob")
 	}
