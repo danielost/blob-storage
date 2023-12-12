6b8374b91160062281430a25dd183740ad5f8d32
Danylo Ostapchenko <danya.ostap@gmail.com>
2023-12-12 19:23:00 +0200

    Add horizon operations

--- internal/data/horizon/blobs.go
+++ internal/data/horizon/blobs.go
@@ -1,25 +1,110 @@
 package horizon
 
 import (
-	"gitlab.com/distributed_lab/logan/v3"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/pkg/errors"
+	"gitlab.com/distributed_lab/kit/pgdb"
+	"gitlab.com/tokend/go/xdrbuild"
 	"gitlab.com/tokend/horizon-connector"
+	regources "gitlab.com/tokend/regources/generated"
 )
 
-type BlobsQ struct {
-	log     *logan.Entry
-	horizon *horizon.Connector
+// iota is used in case if we need more types in the future
+const (
+	BLOB = iota
+)
+
+type Blob struct {
+	Value   json.RawMessage
+	OwnerId int64
 }
 
-type voidParams struct {
+func (blob Blob) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Value   json.RawMessage `json:"value"`
+		OwnerId int64           `json:"owner_id"`
+	}{
+		Value:   blob.Value,
+		OwnerId: blob.OwnerId,
+	})
 }
 
-func (p *voidParams) Encode() string {
-	return ""
+type BlobsOps struct {
+	Tx      *xdrbuild.Transaction
+	Horizon *horizon.Connector
 }
 
-func NewBlobsQ(log *logan.Entry, horizon *horizon.Connector) *BlobsQ {
-	return &BlobsQ{
-		horizon: horizon,
-		log:     log,
+func New(tx *xdrbuild.Transaction, horizon *horizon.Connector) BlobsOps {
+	return BlobsOps{
+		Tx:      tx,
+		Horizon: horizon,
 	}
+}
+
+func (ops BlobsOps) Create(blob Blob, waitForIngest *bool) (int, []byte, error) {
+	tx := ops.Tx
+	tx = tx.Op(&xdrbuild.CreateData{
+		Type:  BLOB,
+		Value: blob,
+	})
+
+	envelope, err := tx.Marshal()
+	if err != nil {
+		return http.StatusInternalServerError, nil, errors.Wrap(err, "failed to build tx envelope")
+	}
+
+	status, response, err := ops.Horizon.Client().PostJSON("/v3/transactions", &regources.SubmitTransactionBody{
+		Tx:            envelope,
+		WaitForIngest: waitForIngest,
+	})
+	if err != nil {
+		return http.StatusInternalServerError, nil, errors.Wrap(err, "failed to insert blob")
+	}
+
+	return status, response, nil
+}
+
+func (ops BlobsOps) Get(pageParams pgdb.OffsetPageParams) ([]byte, error) {
+	endpoint := fmt.Sprintf("/v3/data?page[limit]=%d&page[number]=%d&page[order]=%s", pageParams.Limit, pageParams.PageNumber, pageParams.Order)
+	response, err := ops.Horizon.Client().Get(endpoint)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get blob list")
+	}
+
+	return response, nil
+}
+
+func (ops BlobsOps) GetById(id int64) ([]byte, error) {
+	endpoint := fmt.Sprintf("/v3/data/%d", id)
+	response, err := ops.Horizon.Client().Get(endpoint)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get blob")
+	}
+
+	return response, nil
+}
+
+func (ops BlobsOps) Delete(id int64, waitForIngest *bool) (int, []byte, error) {
+	tx := ops.Tx
+	tx = tx.Op(&xdrbuild.RemoveData{
+		ID: uint64(id),
+	})
+
+	envelope, err := tx.Marshal()
+	if err != nil {
+		return http.StatusInternalServerError, nil, errors.Wrap(err, "failed to build tx envelope")
+	}
+
+	status, response, err := ops.Horizon.Client().PostJSON("/v3/transactions", &regources.SubmitTransactionBody{
+		Tx:            envelope,
+		WaitForIngest: waitForIngest,
+	})
+	if err != nil {
+		return http.StatusInternalServerError, nil, errors.Wrap(err, "failed to delete blob")
+	}
+
+	return status, response, nil
 }
--- internal/helpers/ctx.go
+++ internal/helpers/ctx.go
@@ -6,6 +6,10 @@ import (
 
 	"gitlab.com/dl7850949/blob-storage/internal/data"
 	horizon2 "gitlab.com/dl7850949/blob-storage/internal/data/horizon"
+	"gitlab.com/tokend/go/xdrbuild"
+	"gitlab.com/tokend/horizon-connector"
+	"gitlab.com/tokend/keypair"
+	"gitlab.com/tokend/regources"
 
 	"gitlab.com/distributed_lab/logan/v3"
 )
@@ -17,7 +21,29 @@ const (
 	blobsQCtxKey
 	horizonBlobsQCtxKey
 	usersQCtxKey
+	horizonConnectorCtxKey
+	txBuilderCtxKey
+	coreInfoCtxKey
 )
+
+func BlobsOps(r *http.Request) horizon2.BlobsOps {
+	txbuilderbuilder := r.Context().Value(txBuilderCtxKey).(data.Infobuilder)
+	info := r.Context().Value(coreInfoCtxKey).(data.Info)
+	coreInfo, err := info.Info()
+	master, err := keypair.ParseAddress(coreInfo.MasterAccountID)
+
+	if err != nil {
+		//TODO handle error
+		panic(err)
+	}
+
+	tx := txbuilderbuilder(info, master)
+
+	return horizon2.New(
+		tx,
+		Horizon(r),
+	)
+}
 
 func CtxLog(entry *logan.Entry) func(context.Context) context.Context {
 	return func(ctx context.Context) context.Context {
@@ -29,24 +55,16 @@ func Log(r *http.Request) *logan.Entry {
 	return r.Context().Value(logCtxKey).(*logan.Entry)
 }
 
+// Postgres
 func CtxBlobsQ(entry data.BlobsQ) func(context.Context) context.Context {
 	return func(ctx context.Context) context.Context {
 		return context.WithValue(ctx, blobsQCtxKey, entry)
 	}
 }
 
+// Postgres
 func BlobsQ(r *http.Request) data.BlobsQ {
 	return r.Context().Value(blobsQCtxKey).(data.BlobsQ).New()
-}
-
-func CtxHBlobsQ(q *horizon2.BlobsQ) func(context.Context) context.Context {
-	return func(ctx context.Context) context.Context {
-		return context.WithValue(ctx, horizonBlobsQCtxKey, q)
-	}
-}
-
-func HBlobsQ(r *http.Request) *horizon2.BlobsQ {
-	return r.Context().Value(horizonBlobsQCtxKey).(*horizon2.BlobsQ)
 }
 
 func CtxUsersQ(entry data.UsersQ) func(context.Context) context.Context {
@@ -57,4 +75,43 @@ func CtxUsersQ(entry data.UsersQ) func(context.Context) context.Context {
 
 func UsersQ(r *http.Request) data.UsersQ {
 	return r.Context().Value(usersQCtxKey).(data.UsersQ).New()
+}
+
+func CtxHorizon(q *horizon.Connector) func(context.Context) context.Context {
+	return func(ctx context.Context) context.Context {
+		return context.WithValue(ctx, horizonConnectorCtxKey, q)
+	}
+}
+
+func Horizon(r *http.Request) *horizon.Connector {
+	return r.Context().Value(horizonConnectorCtxKey).(*horizon.Connector).Clone()
+}
+
+func CtxCoreInfo(s data.Info) func(context.Context) context.Context {
+	return func(ctx context.Context) context.Context {
+		return context.WithValue(ctx, coreInfoCtxKey, s)
+	}
+}
+
+func CoreInfo(r *http.Request) *regources.Info {
+	info, err := r.Context().Value(coreInfoCtxKey).(data.Info).Info()
+	if err != nil {
+		//TODO handle error
+		panic(err)
+	}
+	return info
+}
+
+func CtxTransaction(txbuilder data.Infobuilder) func(context.Context) context.Context {
+	return func(ctx context.Context) context.Context {
+		ctx = context.WithValue(ctx, txBuilderCtxKey, txbuilder)
+		return ctx
+	}
+}
+
+func Transaction(r *http.Request) *xdrbuild.Transaction {
+	txbuilderbuilder := r.Context().Value(txBuilderCtxKey).(data.Infobuilder)
+	info := r.Context().Value(coreInfoCtxKey).(data.Info)
+	master := keypair.MustParseAddress(CoreInfo(r).MasterAccountID)
+	return txbuilderbuilder(info, master)
 }
